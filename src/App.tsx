958834c32c57d98de236b9395cff9dcaae163281
20002477 <VVasiGoydin@sberbank.ru>
2023-02-06 18:00:27 +0300

    add react-route-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from './components/Container/Container';
 import { ConfigProvider } from 'antd/lib';
 import { ThemeConfig } from 'antd/es/config-provider/context';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
 
 function App() {
   const theme: ThemeConfig = {
@@ -19,7 +20,14 @@ function App() {
         },
       }}
     >
-      <Container />
+      <Routes>
+        <Route path="/" element={<Container />}>
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<div>404 no found</div>} />
+        </Route>
+      </Routes>
     </ConfigProvider>
   );
 }
